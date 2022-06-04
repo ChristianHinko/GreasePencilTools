fc699e3c8e6e84f6abc65d76d1dc0b0e67768a0a
Christian Hinkle <c4hinkle@gmail.com>
2022-06-04 17:50:26 -0400

    delete PointLiesOnTriangle() and instead use blender's intersect_point_tri() from mathutils.geometry

--- PerspectiveTools/CalculateNormalOfPerspectivePlane.py
+++ PerspectiveTools/CalculateNormalOfPerspectivePlane.py
@@ -12,34 +12,40 @@ class CalculateNormalOfPerspectivePlane(bpy.types.Operator):
     
     def execute(self, context):
         # Get the selected points
-        SelectedPoints: list[bpy.types.GPencilStrokePoint] = GetGreasePencilStrokePoints()
+        StrokePoints: list[bpy.types.GPencilStrokePoint] = GetGreasePencilStrokePoints()
         
         # Ensure we are working with 4 vertices
-        if (len(SelectedPoints) != 4):
+        if (len(StrokePoints) != 4):
             print("Must be used with exactly 4 points!")
             return {'CANCELLED'}
         
+        # List of vectors
+        Points: list[mathutils.Vector] = [ ]
+        for StrokePoint in StrokePoints:
+            Points.append(StrokePoint.co)
+        
         # The points of this plane
-        PointA: mathutils.Vector = SelectedPoints[0]
-        PointB: mathutils.Vector = SelectedPoints[1]
-        PointC: mathutils.Vector = SelectedPoints[2]
-        PointD: mathutils.Vector = SelectedPoints[3]
+        PointA: mathutils.Vector = Points[0]
+        PointB: mathutils.Vector = Points[1]
+        PointC: mathutils.Vector = Points[2]
+        PointD: mathutils.Vector = Points[3]
         
         # Ensure points are coplanar
-        if (PointsAreCoplanar([PointA.co, PointB.co, PointC.co, PointD.co]) == False):
+        if (PointsAreCoplanar([PointA, PointB, PointC, PointD]) == False):
             print("The 4 points are not aligned on the same global plane!")
             return {'CANCELLED'}
         
-        # Ensure quad is convex
-        if (PointLiesOnTriangle(PointA.co, PointB.co, PointC.co, PointD.co)):
+        # Ensure that quad is convex
+        if (mathutils.geometry.intersect_point_tri(PointD, PointA, PointB, PointC) != None):
             print("The 4 points are not convex!")
             return {'CANCELLED'}
         
+        
         # The vectors between the different points
-        AToB: mathutils.Vector = (PointB.co - PointA.co)
-        BToC: mathutils.Vector = (PointC.co - PointB.co)
-        CToD: mathutils.Vector = (PointD.co - PointC.co)
-        DToA: mathutils.Vector = (PointA.co - PointD.co)
+        AToB: mathutils.Vector = (PointB - PointA)
+        BToC: mathutils.Vector = (PointC - PointB)
+        CToD: mathutils.Vector = (PointD - PointC)
+        DToA: mathutils.Vector = (PointA - PointD)
         
         # The normal of this plane in global space
         GlobalNormal: mathutils.Vector = AToB.normalized().cross(BToC.normalized())
@@ -54,7 +60,6 @@ def menu_func(self, context):
 # Update the register function to show up in a menu
 bpy.utils.register_class(CalculateNormalOfPerspectivePlane)
 bpy.types.VIEW3D_MT_view.append(menu_func)
-
 
 ###
  # Gets the currently selected grease pencil stroke points - if any
@@ -86,83 +91,6 @@ def GetGreasePencilStrokePoints() -> list[bpy.types.GPencilStrokePoint]:
         
     # Return out gathered points
     return SelectedPoints
-
-###
- # Given two bounding directions A and B and a test direction, does the direction lie between them?
- # Only can return true if the three directions are coplanar.
- ##
-def DirectionIsBetween(InA: mathutils.Vector, InB: mathutils.Vector, bInInclusive: bool, InDirection: mathutils.Vector, InErrorTolerance: float = 1e-4) -> bool:
-    # Get the normals
-    # 
-    # InA ^
-    #     |        InDirection
-    #     |          ^
-    #     | NormalA /
-    #     |        /
-    #     |       /
-    #     |      /
-    #     |     /
-    #     |    /
-    #     |   /  NormalB
-    #     |  /
-    #     | /
-    #     |/_____________>
-    #                    InB
-    # 
-    NormalA: mathutils.Vector = InA.cross(InDirection).normalized()
-    NormalB: mathutils.Vector = InDirection.cross(InB).normalized()
-    
-    # If we are inclusive, check if the direction is on one of the bounding directions
-    if (bInInclusive):
-        # Cross product of zero means that the direction is on the bound
-        if (IsNearlyEqualVector(NormalA, mathutils.Vector((0, 0, 0)), InErrorTolerance)
-            or IsNearlyEqualVector(NormalB, mathutils.Vector((0, 0, 0)), InErrorTolerance)):
-            return true;
-    
-    # If the two normals are not opposites, then we are within the bounding directions
-    bSameNormals: bool = IsNearlyEqualFloat(NormalA.dot(NormalB), 1, InErrorTolerance)
-    return bSameNormals;
-
-###
- # Given a triangle and a point, does that point lie on the triangle?
- ##
-def PointLiesOnTriangle(InA: mathutils.Vector, InB: mathutils.Vector, InC: mathutils.Vector, InPoint: mathutils.Vector, InErrorTolerance: float = 1e-4) -> bool:
-    # Whether the point is within A's angle
-    AToPoint: mathutils.Vector = (InPoint - InA)
-    AToB: mathutils.Vector = (InB - InA)
-    AToC: mathutils.Vector = (InC - InA)
-    bPointIsBetweenBAndC: bool = DirectionIsBetween(AToB, AToC, True, AToPoint, InErrorTolerance)
-    
-    # Whether the point is within B's angle
-    BToPoint: mathutils.Vector = (InPoint - InB)
-    BToA: mathutils.Vector = (InA - InB)
-    BToC: mathutils.Vector = (InC - InB)
-    bPointIsBetweenAAndC: bool = DirectionIsBetween(BToA, BToC, True, BToPoint, InErrorTolerance)
-    
-    if (bPointIsBetweenBAndC and bPointIsBetweenAAndC):
-        # Point is on the triangle
-        # 
-        #          C
-        #         / \
-        #        /   \
-        #       / o   \
-        #      /       \
-        #     /         \
-        #    A _________ B
-        # 
-        return True
-    
-    # Point is not on the triangle
-    # 
-    #          C
-    #         / \
-    #      o /   \
-    #       /     \
-    #      /       \
-    #     /         \
-    #    A _________ B
-    # 
-    return False;
 
 ###
  # Given four points, do they exist on the same plane?
